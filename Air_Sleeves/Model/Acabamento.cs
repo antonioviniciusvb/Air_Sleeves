9b5b90ae8cb4419f2efc3300f953ddcbaf60a76b
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2024-04-25 02:03:37 -0300

    Limpeza de código classe Acabamento.

--- Air_Sleeves/Model/Acabamento.cs
+++ Air_Sleeves/Model/Acabamento.cs
@@ -1,62 +1,29 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using PropertyChanged;
-using Air_Sleeves.Util;
+﻿using Air_Sleeves.Util;
 
 namespace Air_Sleeves.Model
 {
     public class Acabamento: Camisa
     {
-        decimal ft_Acabamento = 0.00000011M;
+        const decimal ft_Acabamento = 0.00000011M;
         public Acabamento()
         {
 
         }
-        public Acabamento(Camisa camisa_1, Isopor isopor)
+        private decimal CalculaCompostoResina(Camisa camisa_1, Isopor isopor)
         {
-           this.Composto_Resina =  Calc_Composto_Resina(camisa_1, isopor);
-        }
-        private decimal Calc_Composto_Resina(Camisa c_1, Isopor isopor)
-        {
-            decimal ext_1_quadrado = Calculo.Eleva_Ao_Quadrado(c_1.Externa + 1);
+            decimal ext_1_quadrado = Calculo.Eleva_Ao_Quadrado(camisa_1.Externa + 1);
             decimal ext_3_quadrado = Calculo.Eleva_Ao_Quadrado(isopor.Externa - 0.4M);
 
-            return Calculo.Multiplica((Calculo.pi / 4), c_1.Comprimento, (ext_1_quadrado - ext_3_quadrado), ft_Acabamento, 2.0M, 3);
-        }
-
-        public void Calc_Valores_Acabamento()
-        {
-            LimpaTotais();
-            CalculaItens();
+            return Calculo.Multiplica((Calculo.pi / 4), camisa_1.Comprimento, (ext_1_quadrado - ext_3_quadrado), ft_Acabamento, 2.0M, 3);
         }
 
         public void CalculaValores(Camisa camisa_1, Isopor isopor)
         {
             LimpaTotais();
-            Composto_Resina = Calc_Composto_Resina(camisa_1, isopor);
+
+            //Isso irá aumentar o custo em 25% de todos os produtos
+            Composto_Resina = Calculo.Adiciona_Percentual(CalculaCompostoResina(camisa_1, isopor), 25,3);
             CalculaItens();
-        }
-
-
-        public void Adiciona25pct()
-        {
-            Peso_Resina = Calculo.Adiciona_Percentual(Peso_Resina, 25, 3);
-            Peso_HT231 = Calculo.Adiciona_Percentual(Peso_HT231, 25, 3);
-            Peso_AntiBolha = Calculo.Adiciona_Percentual(Peso_AntiBolha, 25, 3);
-            Peso_K10 = Calculo.Adiciona_Percentual(Peso_K10, 25, 3);
-            Peso_Pigmento = Calculo.Adiciona_Percentual(Peso_Pigmento, 25, 3);
-            Composto_Resina = Calculo.Adiciona_Percentual(Composto_Resina, 25, 3);
-
-            Preco_Resina = Calculo.Adiciona_Percentual(Preco_Resina, 25, 3);
-            Preco_HT231 = Calculo.Adiciona_Percentual(Preco_HT231, 25, 3);
-            Preco_AntiBolha = Calculo.Adiciona_Percentual(Preco_AntiBolha, 25, 3);
-            Preco_K10 = Calculo.Adiciona_Percentual(Preco_K10, 25, 3);
-            Preco_Pigmento = Calculo.Adiciona_Percentual(Preco_Pigmento, 25, 3);
-
-
-            Preco_Total = Calculo.Adiciona_Percentual(Preco_Total, 25, 3);
-            Peso_Total = Calculo.Adiciona_Percentual(Peso_Total, 25, 3);
         }
     }
 }
